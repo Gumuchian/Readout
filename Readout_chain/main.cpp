be71038c7d9290d49b1a3ac0919322c8cd27518a
Gumuchian <paulgumuchian@yahoo.fr>
2017-06-07 18:19:27 +0200

    MAJ

--- Readout_chain/main.cpp
+++ Readout_chain/main.cpp
@@ -17,7 +17,8 @@ int main()
         ch0.computeBBFB();
     }*/
     Pixel pix(1000000,0,0,1,20,20000000,1000000);
-    int i,Gf=10000;
+    DDS dds(pow(2,9),pow(2,18),pow(2,7));
+    int i,Gf=100000;
     double dac,LC,fbck;
     ofstream fichier("test.txt", ios::out);
     if(fichier)
@@ -33,7 +34,7 @@ int main()
             LC=pix.computeLC();
             fbck=0.5*0.01/pow(2,15)*((Gf*pix.getfeedback())>>19);
             dac=0.5*80*0.0017/(5.8*pow(10,-6))*(LC-0.1*fbck);
-            pix.computeBBFB(trunc(pow(2,18)*cos(2*M_PI*1.0/20.0*i)),trunc(pow(2,18)*cos(2*M_PI*1.0/20.0*i)),trunc(pow(2,18)*sin(2*M_PI*1.0/20.0*i)),trunc(pow(2,18)*sin(2*M_PI*1.0/20.0*i)),trunc(pow(2,12)*dac),1000000);
+            pix.computeBBFB(trunc(pow(2,18)*cos(2*M_PI*1.0/20.0*(i-1))),trunc(pow(2,18)*cos(2*M_PI*1.0/20.0*i)),trunc(pow(2,18)*sin(2*M_PI*1.0/20.0*(i-1))),trunc(pow(2,18)*sin(2*M_PI*1.0/20.0*i)),trunc(pow(2,12)*dac),1000000);
             fichier << dac << endl;
         }
         fichier.close();
