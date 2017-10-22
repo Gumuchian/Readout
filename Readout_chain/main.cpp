0d32727ec312cdafd832b1b6ca4ba1a4c1f04907
Gumuchian <paulgumuchian@yahoo.fr>
2017-10-22 16:47:53 +0200

    MAJ

--- Readout_chain/main.cpp
+++ Readout_chain/main.cpp
@@ -24,14 +24,14 @@ int main()
     //CIC cic;
     Butterworth Butter;
     Pulse_generator pulse_generator;
-    int i,j,k,m,ip=0,l=0,n=0;
+    int i,j,k,m,ip=0,l=0,n=0,ind=1;
     vector<double> module(Npat,0);
     vector<double> E;
     string str;
     //char* ptr;
-    double sum,Em=0,var=0,P=0,maxi,a;
-    double pulse[Np],puls;
-    double pattern[Npat][3];
+    double sum,Em=0,var=0,P=0,maxi,a,P1=0,P2=0,P3=0,Energ;
+    double pulse[Npul],puls,sum_I=0;
+    double pattern[8192][3];
     ofstream file1("test.txt", ios::out);
     ifstream file("Pattern.txt", ios::out);
 
@@ -42,7 +42,7 @@ int main()
             pulse_generator.setPopt(energy);
         }
         puls=pulse_generator.compute();
-        if (i>=1000000 && i<1000000+Np)
+        if (i>=1000000 && i<1000000+Npul)
         {
             pulse[ip]=puls;
             ip++;
@@ -50,17 +50,32 @@ int main()
     }
     ip=0;
 
-    for (i=0;i<Np/decimation+1;i++)
+    for (i=0;i<8192;i++)
     {
-        P=P+pow(pattern[i],2);
+        for (j=0;j<3;j++)
+        {
+            file >> pattern[i][j];
+        }
     }
+
+    for (i=0;i<Npat;i++)
+    {
+        pattern[i][ind]=pattern[0][0]-pattern[i][ind];
+        P=P+pow(pattern[i][ind],2);
+        sum_I=sum_I+pattern[i][ind];
+    }
+
+    Energ=sum_I*Vp*decimation/(1.6*pow(10,-19)*fs);
 
     for (i=0;i<N;i++)
     {
         // sumPolar = bias sum of each pixel
         ch0.sumPolar();
         // bias modulation by pulse
-        ch0.setI(pulse[ip]);
+        if (ip<Npul)
+        {
+            ch0.setI(pulse[ip]);
+        }
         // compute LC_TES = output of LC-TES
         ch0.computeLC_TES();
         // compute feedback
@@ -82,21 +97,18 @@ int main()
                 if (l==0)
                 {
                     sum=0;
-                    for (k=0;k<Np/decimation+1;k++)
+                    for (k=0;k<Npat;k++)
                     {
-                        sum=(module[Np/decimation-k]*G_filter*PE/pow(2,DAC_bit)*Gb/Npr*0.1*TR/sqrt(2))*pattern[k]+sum;
+                        sum=(module[k]*G_filter*PE/pow(2,DAC_bit)*Gb/Npr*0.1*TR/sqrt(2))*pattern[k][ind]+sum;
                     }
-                    E.push_back(energy*sum/P);
-                    n++;
-                    if (n==4){m=1;}else{m=0;}
-                    n=n%4;
+                    E.push_back(Energ*sum/P);
                 }
                 l++;
-                l=l%(Np/decimation+1-m);
+                l=l%Npat;
             }
         }
         ip++;
-        ip=ip%Np;
+        ip=ip%(Npat*decimation);
     }
 
     for (i=3;i<(int)E.size();i++)
@@ -110,7 +122,10 @@ int main()
         var=pow(abs(E[i])-Em,2)+var;
     }
 
-    cout << "Input energy: " << energy << " eV" << endl << "Energy estimation: " << Em << " eV" << endl << "Relative error: " << abs(energy-Em)/energy << endl;
+    cout << "Input energy: " << energy << " eV" << endl;
+    cout << "Energy estimation: " << Em << " eV" << endl;
+    cout << "Relative error: " << abs(energy-Em)/energy << endl;
     cout << "Resolution: " << 2.35*sqrt(var/(E.size()-3)) << " eV" << endl;
+    cout << "Number of estimations: " << E.size()-3 << endl;
     return 0;
 }
