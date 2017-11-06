545e0c02a49518c694e42621f980d4e3dce36c1b
Gumuchian <paulgumuchian@yahoo.fr>
2017-11-06 15:55:01 +0100

    MAJ

--- Readout_chain/src/BBFB.cpp
+++ Readout_chain/src/BBFB.cpp
@@ -13,7 +13,7 @@ void BBFB::compute_feedback(double demoduI, double remoduI, double demoduQ, doub
 {
     I+=G*trunc(demoduI*input/Npr);
     Q+=G*trunc(demoduQ*input/Npr);
-    feedback = trunc((remoduI*I+remoduQ*Q)/Npr);
+    feedback = pow(2,DAC_bit-ADC_bit)*trunc((remoduI*I+remoduQ*Q)/Npr);
 }
 
 double BBFB::getfeedback()
@@ -23,24 +23,5 @@ double BBFB::getfeedback()
 
 double BBFB::module()
 {
-    /*int i,sigma;
-    int u0[2]={I,Q};
-    int u1[2];
-    if (u0[0]*u1[0]<0){
-        sigma = 1;
-    }
-    else{
-        sigma = -1;
-    }
-    for (i=0;i<10;i++){
-        u1[0] = u0[0] - ((sigma*u0[1])>>i);
-        u1[1] = u0[1] + ((sigma*u0[0])>>i);
-        if (u0[0]*u1[0]<0){
-            sigma *=-1;
-        }
-        u0[0]=u1[0];
-        u0[1]=u1[1];
-    }*/
-    //return 0.60725*u1[1];
-    return sqrt(pow(I,2)+pow(Q,2));
+    return trunc(sqrt(pow(I,2)+pow(Q,2)));
 }
